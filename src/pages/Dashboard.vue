2c322e64fabaa83de9d544e86754d192a2cfd2eb
g-vinokurov <g.vinokurov@g.nsu.ru>
2024-05-21 01:24:22 +0700

    Update Dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -7,6 +7,7 @@
       DashboardLogo,
       DashboardShowSidebar
     },
+    
     // hack for component-dependent html tag 'body' styling
     beforeCreate: function() {
         document.body.className = 'dashboard'
@@ -33,13 +34,27 @@
           <DashboardLogo/>
         </div>
         <div class="interstellarium-dashboard-sidebar-content">
-          <div class="interstellarium-dashboard-card col-12 col-sm-6 col-md-12"></div>
-          <div class="interstellarium-dashboard-card col-12 col-sm-6 col-md-12"></div>
-          <div class="interstellarium-dashboard-card col-12 col-sm-6 col-md-12"></div>
-          <div class="interstellarium-dashboard-card col-12 col-sm-6 col-md-12"></div>
-          <div class="interstellarium-dashboard-card col-12 col-sm-6 col-md-12"></div>
-          <div class="interstellarium-dashboard-card col-12 col-sm-6 col-md-12"></div>
-          <div class="interstellarium-dashboard-card col-12 col-sm-6 col-md-12"></div>
+          <div class="interstellarium-dashboard-card col-12 col-sm-6 col-md-12">
+            <a href="#" class="interstellarium-dashboard-card-link">Персонал</a>
+          </div>
+          <div class="interstellarium-dashboard-card col-12 col-sm-6 col-md-12">
+            <a href="#" class="interstellarium-dashboard-card-link">Отделы</a>
+          </div>
+          <div class="interstellarium-dashboard-card col-12 col-sm-6 col-md-12">
+            <a href="#" class="interstellarium-dashboard-card-link">Проекты</a>
+          </div>
+          <div class="interstellarium-dashboard-card col-12 col-sm-6 col-md-12">
+            <a href="#" class="interstellarium-dashboard-card-link">Договоры</a>
+          </div>
+          <div class="interstellarium-dashboard-card col-12 col-sm-6 col-md-12">
+            <a href="#" class="interstellarium-dashboard-card-link">Группы</a>
+          </div>
+          <div class="interstellarium-dashboard-card col-12 col-sm-6 col-md-12">
+            <a href="#" class="interstellarium-dashboard-card-link">Оборудование</a>
+          </div>
+          <div class="interstellarium-dashboard-card col-12 col-sm-6 col-md-12">
+            <a href="#" class="interstellarium-dashboard-card-link">Работы</a>
+          </div>
         </div>
         <div class="interstellarium-dashboard-footer">
           <DashboardShowSidebar/>
