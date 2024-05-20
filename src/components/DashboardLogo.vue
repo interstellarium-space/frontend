7fe475c2ef045f35359cb1c2323217681c4d445b
g-vinokurov <g.vinokurov@g.nsu.ru>
2024-05-21 00:18:18 +0700

    Update Dashboard

---
+++ src/components/DashboardLogo.vue
@@ -0,0 +1,37 @@
+<script>
+  export default {
+  
+  }
+</script>
+
+<template>
+  <div class="interstellarium-dashboard-logo-wrapper">
+    <div class="interstellarium-dashboard-logo m-2">
+      INTERSTELLARIUM
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.interstellarium-dashboard-logo {
+    font-family: var(--interstellarium-special-font-family), sans-serif;
+    font-size: max(2vw, 3.2vh);
+    font-weight: 900;
+    text-align: center;
+    background: var(--interstellarium-gradient);
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    color: var(--interstellarium-primary);
+}
+
+@media (min-width: 768px) {
+    .interstellarium-dashboard-logo {
+        font-size: 2vw;
+    }
+  
+    .interstellarium-dashboard-main .interstellarium-dashboard-logo-wrapper {
+        display: none;
+    }
+}
+</style>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,8 +1,9 @@
 <script>
   import DashboardShowSidebar from "../components/DashboardShowSidebar.vue";
+  import DashboardLogo from "../components/DashboardLogo.vue";
   
   export default {
-    components: {DashboardShowSidebar},
+    components: {DashboardLogo, DashboardShowSidebar},
     // hack for component-dependent html tag 'body' styling
     beforeCreate: function() {
         document.body.className = 'dashboard'
@@ -25,11 +26,17 @@
   <div class="interstellarium-container">
     <div class="interstellarium-dashboard">
       <div class="interstellarium-dashboard-sidebar" id="dashboard-sidebar">
+        <div class="interstellarium-dashboard-header">
+          <DashboardLogo/>
+        </div>
         <div class="interstellarium-dashboard-footer">
           <DashboardShowSidebar/>
         </div>
       </div>
       <div class="interstellarium-dashboard-main" id="dashboard-main">
+        <div class="interstellarium-dashboard-header">
+          <DashboardLogo/>
+        </div>
         <div class="interstellarium-dashboard-footer">
           <DashboardShowSidebar/>
         </div>
@@ -57,27 +64,4 @@
 <style scoped>
 @import "/src/styles/style.css";
 @import "/src/styles/dashboard.css";
-
-/*
-.interstellarium-dashboard-footer {
-    display: flex;
-    align-items: center;
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    background: white;
-    padding-bottom: .5rem;
-    padding-top: .5rem;
-}
-
-@media (min-width: 768px) {
-}
-
-@media (min-width: 992px) {
-}
-
-@media (min-width: 1200px) {
-}
-*/
 </style>
