16a9c9d3c2ead79b614f7f83881a34b3b0334af6
g-vinokurov <g.vinokurov@g.nsu.ru>
2024-05-30 18:02:16 +0700

    Add Contracts and Projects

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,9 +1,17 @@
 <script>
+  import axios from "axios";
+  
+  import {prepareAPIRequest} from "../services/index.js";
+
   import DashboardSidebar from "../components/dashboard/Sidebar.vue";
   import DashboardMain from "../components/dashboard/Main.vue";
+  import DashboardSearchArea from "../components/dashboard/SearchArea.vue";
+  import DashboardCreateEntity from "../components/dashboard/CreateEntity.vue";
   
   export default {
     components: {
+      DashboardCreateEntity,
+      DashboardSearchArea,
       DashboardSidebar,
       DashboardMain
     },
@@ -15,15 +23,66 @@
     },
     
     beforeRouteLeave: function () {
-        document.body.classList.remove('dashboard')
-        document.getElementById('app').classList.remove('dashboard')
+      document.body.classList.remove('dashboard')
+      document.getElementById('app').classList.remove('dashboard')
     },
     
     setup() {
       document.title = 'Проекты | Interstellarium'
     },
     
+    data() {
+      return {
+        searchFilters: {
+          name: null,
+          start_date: null,
+          finish_date: null
+        },
+        projects: []
+      }
+    },
+    
     methods: {
+      prepareSearchFilters() {
+        if (this.searchFilters.name === '')
+          this.searchFilters.name = null
+        if (this.searchFilters.start_date === '')
+          this.searchFilters.start_date = null
+        if (this.searchFilters.finish_date === '')
+          this.searchFilters.finish_date = null
+      },
+      
+      async searchProjects() {
+        let req = prepareAPIRequest('/api/projects')
+        
+        this.prepareSearchFilters()
+        
+        let payload = {
+          name: this.searchFilters.name,
+          start_date: this.searchFilters.start_date,
+          finish_date: this.searchFilters.finish_date,
+        }
+        
+        console.log(payload)
+        
+        let router = this.$router
+        
+        const res = await axios.post(req.url, payload, req.config).catch(
+            function (error) {
+              if (error.response.status === 401) {
+                router.push({name: 'Logout'})
+              }
+            }
+        )
+        
+        console.debug(res)
+        
+        if (res && res.status === 200) {
+          this.projects = res.data
+          console.log(res.data)
+        }
+      },
+      
       mounted() {
         document.title = 'Проекты | Interstellarium'
       }
@@ -36,7 +95,82 @@
     <div class="interstellarium-dashboard">
       <DashboardSidebar></DashboardSidebar>
       <DashboardMain>
-        <div></div>
+        <template v-slot:search-area>
+          <DashboardSearchArea>
+            <template v-slot:main-line-fields>
+              <div class="col-5 col-sm-8 col-lg-9 my-2 my-md-1 px-1 px-sm-2">
+                <input
+                    type="text"
+                    class="form-control"
+                    id="filter-name"
+                    placeholder="Введите наименование проекта"
+                    v-model="searchFilters.name"
+                >
+              </div>
+              <div class="col-4 col-sm-2 my-2 my-md-1 px-1 px-sm-2">
+                <input
+                    @click="this.searchProjects()"
+                    type="submit"
+                    class="form-control btn btn-interstellarium"
+                    id="search"
+                    value="Поиск"
+                >
+              </div>
+            </template>
+            <template v-slot:filters>
+              <div
+                  class="col-6 my-2 my-md-1 px-1 px-sm-2 interstellarium-dashboard-search-filter hidden">
+                <input
+                    type="text"
+                    class="form-control"
+                    id="filter-start-date"
+                    placeholder="Начат с даты"
+                    onfocus="this.type='date'"
+                    v-model="searchFilters.start_date"
+                >
+              </div>
+              <div
+                  class="col-6 my-2 my-md-1 px-1 px-sm-2 interstellarium-dashboard-search-filter hidden">
+                <input
+                    type="text"
+                    class="form-control"
+                    id="filter-finish-date"
+                    placeholder="Завершен до даты"
+                    onfocus="this.type='date'"
+                    v-model="searchFilters.finish_date"
+                >
+              </div>
+            </template>
+          </DashboardSearchArea>
+        </template>
+        
+        <template v-slot:content>
+          <div v-for="project in this.projects"
+               class="interstellarium-dashboard-main-content-card mb-3">
+            <div class="interstellarium-dashboard-main-content-link">
+              {{ project.name }}
+            </div>
+            <div v-if="project.chief.id != null"
+                 class="interstellarium-dashboard-main-content-text">
+              Руководитель: {{ project.chief.name }}
+            </div>
+            <div v-else class="interstellarium-dashboard-main-content-text">
+              Руководитель: не назначен
+            </div>
+            <div v-if="project.group.id != null"
+                 class="interstellarium-dashboard-main-content-text">
+              Рабочая группа: {{ project.group.name }}
+            </div>
+            <div v-else class="interstellarium-dashboard-main-content-text">
+              Рабочая группа: не назначена
+            </div>
+          </div>
+        </template>
+        
+        <template v-slot:create-entity>
+          <DashboardCreateEntity
+              @click="this.$router.push({name: 'CreateProject'})"/>
+        </template>
       </DashboardMain>
     </div>
   </div>
