b2f721fb9c32b172234f1a5498e38de6be65c9ef
g-vinokurov <g.vinokurov@g.nsu.ru>
2024-06-07 14:07:05 +0700

    Update Project Profile, README.md

--- README.md
+++ README.md
@@ -8,31 +8,44 @@
 ![Reset Password](./docs/resources/reset-password.png "Reset Password")
 ## Dashboard page
 ![Dashboard](./docs/resources/dashboard.png "Dashboard")
+
 ## Users page
 ![Users](./docs/resources/users.png "Users")
 ## Create User page
 ![Create User](./docs/resources/create-user.png "Create User")
+
 ## Groups page
 ![Groups](./docs/resources/groups.png "Groups")
 ## Create Group page
 ![Create Group](./docs/resources/create-group.png "Create Group")
+
 ## Works page
 ![Works](./docs/resources/works.png "Works")
 ## Create Work page
 ![Create Work](./docs/resources/create-work.png "Create Work")
+
 ## Equipment page
 ![Equipment](./docs/resources/equipment.png "Equipment")
 ## Create Equipment page
 ![Create Equipment](./docs/resources/create-equipment.png "Create Equipment")
+
 ## Departments page
 ![Departments](./docs/resources/departments.png "Departments")
 ## Create Department page
 ![Create Department](./docs/resources/create-department.png "Create Department")
+## Department Profile page
+![Department Profile](./docs/resources/department-profile.png "Department Profile")
+
 ## Projects page
 ![Projects](./docs/resources/projects.png "Projects")
 ## Create Project page
 ![Create Project](./docs/resources/create-project.png "Create Project")
+## Project Profile page
+![Project Profile](./docs/resources/project-profile.png "Project Profile")
+
 ## Contracts page
 ![Contracts](./docs/resources/contracts.png "Contracts")
 ## Create Contract page
 ![Create Contract](./docs/resources/create-contract.png "Create Contract")
+## Contract Profile page
+![Contract Profile](./docs/resources/contract-profile.png "Contract Profile")
--- src/pages/dashboard/projects/ProjectProfile.vue
+++ src/pages/dashboard/projects/ProjectProfile.vue
@@ -4,6 +4,8 @@
   import Header from "../../../components/dashboard/Header.vue";
   import Footer from "../../../components/dashboard/Footer.vue";
 
+  import { getUser, isAdmin } from "../../../services/Auth.js";
+
   import { APIProjectsProfile } from "../../../services/api/projects/Profile.js";
 
   export default {
@@ -16,11 +18,63 @@
 
     setup() {
       document.title = "Interstellarium"
+      let userIsAdmin = isAdmin(getUser())
+      return {userIsAdmin}
     },
 
-    async mounted() {
-      let response = await APIProjectsProfile(this.$route.params.projectId);
-      console.log(response)
+    data() {
+      return {
+        project: {
+          id: 0,
+          name: "",
+          start_date: null,
+          finish_date: null,
+          chief: {
+            id: 0,
+            name: "",
+          },
+          group: {
+            id: 0,
+            name: "",
+          },
+          contracts: [],
+          works: [],
+        },
+        pageIsLoading: false,
+        pageInitIsStarted: false,
+        pageIsReady: false
+      }
+    },
+
+    methods: {
+      async autoload() {
+        if (!this.pageInitIsStarted)
+          return await this.loadData()
+        return false
+      },
+
+      async loadData() {
+        this.pageIsLoading = true
+
+        if (!this.pageInitIsStarted) {
+          this.pageInitIsStarted = true
+
+          let response = await APIProjectsProfile(this.$route.params.projectId);
+
+          if (response.isOk) {
+            this.project = response.data
+          } else {
+            if (response.code === 401) {
+              this.$router.push({name: "AuthLogout"})
+            }
+          }
+
+          console.log(response)
+        }
+
+        this.pageIsLoading = false
+        this.pageIsReady = true
+      }
     }
   }
 </script>
@@ -34,7 +88,88 @@
       <Main>
         <template v-slot:tools></template>
 
-        <template v-slot:content></template>
+        <template v-slot:content>
+          <div v-show="this.pageIsLoading" class="text-center">
+            <div class="spinner-border" role="status">
+              <span class="visually-hidden">Loading...</span>
+            </div>
+          </div>
+
+          <div v-if="this.autoload()"></div>
+
+          <div v-if="this.pageIsReady" class="interstellarium-content-wrapper">
+            <div class="interstellarium-unit-card">
+              <div class="interstellarium-unit-title">
+                {{ this.project.name }}
+              </div>
+            </div>
+            <div class="interstellarium-unit-card">
+              <div v-if="this.project.start_date" class="interstellarium-unit-description">
+                Дата начала: {{ this.project.start_date }}
+              </div>
+              <div v-else class="interstellarium-unit-description">
+                Дата начала: не установлена
+              </div>
+              <div v-if="this.project.finish_date" class="interstellarium-unit-description">
+                Дата завершения: {{ this.project.finish_date }}
+              </div>
+              <div v-else class="interstellarium-unit-description">
+                Дата завершения: не установлена
+              </div>
+            </div>
+            <div class="interstellarium-unit-actionable-card">
+              <div v-if="this.project.chief.id" class="interstellarium-unit-description">
+                Начальник: {{ this.project.chief.name }}
+              </div>
+              <div v-else class="interstellarium-unit-description">
+                Начальник: не назначен
+              </div>
+              <div class="interstellarium-unit-actions mt-3 mt-md-0">
+                <button v-show="this.userIsAdmin" class="btn btn-interstellarium rounded-pill fw-bold px-3">
+                  Назначить
+                </button>
+              </div>
+            </div>
+            <div class="interstellarium-unit-actionable-card">
+              <div v-if="this.project.group.id" class="interstellarium-unit-description">
+                Рабочая группа: {{ this.project.group.name }}
+              </div>
+              <div v-else class="interstellarium-unit-description">
+                Рабочая группа: не назначена
+              </div>
+              <div class="interstellarium-unit-actions mt-3 mt-md-0">
+                <button v-show="this.userIsAdmin" class="btn btn-interstellarium rounded-pill fw-bold px-3">
+                  Назначить
+                </button>
+              </div>
+            </div>
+            <div class="mb-3">
+              <div class="interstellarium-unit-subtitle">
+                Котракты
+              </div>
+            </div>
+            <div class="interstellarium-unit-actionable-card">
+              <div v-if="this.project.contracts.length === 0" class="interstellarium-unit-description">
+                К проекту не привязано ни одного контракта
+              </div>
+              <div class="interstellarium-unit-actions mt-3 mt-md-0">
+                <button v-show="this.userIsAdmin" class="btn btn-interstellarium rounded-pill fw-bold px-3">
+                  + Добавить
+                </button>
+              </div>
+            </div>
+            <div class="mb-3">
+              <div class="interstellarium-unit-subtitle">
+                Проектные работы
+              </div>
+            </div>
+            <div class="interstellarium-unit-actionable-card">
+              <div v-if="this.project.works.length === 0" class="interstellarium-unit-description">
+                По проекту не зарегистрировано ни одной работы
+              </div>
+            </div>
+          </div>
+        </template>
       </Main>
 
       <Footer>
@@ -47,4 +182,21 @@
 <style scoped>
 @import "/src/styles/style.css";
 @import "/src/styles/dashboard.css";
+
+.interstellarium-dashboard-main {
+  top: 5rem;
+}
+
+.interstellarium-unit-title {
+  font-family: var(--interstellarium-work-font-family), sans-serif;
+  color: var(--interstellarium-dark);
+  font-size: 1.5rem;
+}
+
+.interstellarium-unit-subtitle {
+  font-family: var(--interstellarium-work-font-family), sans-serif;
+  color: var(--interstellarium-dark);
+  font-size: 1.1rem;
+}
+
 </style>
