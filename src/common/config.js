d13c5ff0fb622c2fa7b9fb653434866f79c7c322
g-vinokurov <g.vinokurov@g.nsu.ru>
2024-06-08 17:12:02 +0700

    Move config to .env

--- src/common/config.js
+++ src/common/config.js
@@ -1,5 +1,5 @@
 export const projectConfig = {
-    protocol: "http",
-    host: "127.0.0.1",
-    port: "8000"
+    protocol: import.meta.env.VITE_API_PROTOCOL,
+    host: import.meta.env.VITE_API_HOST,
+    port: import.meta.env.VITE_API_PORT
 }
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [vue()],
+    server: {
+      host: env.VITE_HOST,
+      port: env.VITE_PORT
+    }
+  }
 })
